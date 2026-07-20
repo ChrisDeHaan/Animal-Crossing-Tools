28f358a051ae696d0dca574371b0d71c4fd60d9b
Chris <christopherwdehaan@gmail.com>
2026-07-19 21:22:07 -0400

    add base porject features

---
+++ src/app/components/home/home.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+import { TranslocoPipe } from '@jsverse/transloco';
+
+@Component({
+  selector: 'app-home',
+  imports: [TranslocoPipe],
+  templateUrl: './home.html',
+  styleUrl: './home.scss'
+})
+export class Home {}
---
+++ transloco.config.ts
@@ -0,0 +1,9 @@
+import { TranslocoGlobalConfig } from '@jsverse/transloco-utils';
+
+const config: TranslocoGlobalConfig = {
+  rootTranslationsPath: 'assets/i18n/',
+  langs: ['en-us', 'es-us'],
+  keysManager: {}
+};
+
+export default config;
